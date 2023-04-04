a69ebdc72edd7fa3721a17379ccacfe31c00043f
Lucas Jentz <lucasjentz0@gmail.com>
2023-04-04 12:10:12 -0300

    teste tec

---
+++ meusite/java/main.js
@@ -0,0 +1,215 @@
+// console.log("ola pipokinha")
+
+// console.log("primeira bariavel")
+
+// const nome = "lucas"
+// console.log(nome)
+
+// console.log("segunda variavel")
+// let idade = 17
+// console.log(idade)
+// console.log("segunda variavellll")
+
+// idade = 38
+// console.log(idade)
+
+// const idadeAluno = 17
+
+// const nomeAluno = "mano brown"
+
+// const triste = true
+
+// console.log(idadeAluno)
+// console.log(nomeAluno)
+// console.log(triste )
+
+// let nomep = prompt("qual o seu nome")
+// console.log(nomep)
+// let sobrenome = prompt("qual o seu sobrenome")
+// console.log(sobrenome)
+// let idadep = prompt("qual o seu idade")
+// console.log(idadep)
+// let pergunta = prompt("teu pai é casado?")
+// console.log(pergunta)
+
+// if ( pergunta = "sim"){
+//     console.log("mentira")
+// }
+
+
+// const nome = "Lucas"
+// const idade = 17
+
+
+// let nome = prompt("qual o seu nome")
+// let idade = prompt("qual sua idade")
+
+
+// console.log("Meu nome e",nome, "tenho",idade, "anos idade")
+// console.log(`Meu nome e ${nome} e tenho ${idade} anos de idade`)
+
+// console.log(typeof nome)
+
+
+// let num = Number(prompt("escolha um numero"))
+// let num2 = Number(prompt("escolha outro numero"))
+
+// console.log(num)
+// console.log(num2)
+// console.log(typeof num)
+// console.log(typeof num2)
+
+
+// const soma = num + num2
+// console.log(soma)
+
+// if (soma < 5){
+//     console.log("numero pequeno")
+
+// }
+// else{
+//     console.log("numero grande")
+// }
+
+
+// const idade = 2
+
+
+// console.log (typeof idade)
+
+// console.log (typeof idade.toString())
+
+// let frase = "bom dia"
+
+
+// console.log(frase.length)
+// console.log(frase.toLowerCase())
+// console.log(frase.toUpperCase())
+// console.log(frase.trim())
+// console.log(frase.includes("bom"))
+// console.log(frase.replaceAll("bom","mau"))
+
+
+
+// if (frase.length < 10){
+//     console.log("meu pai amado")
+
+// }
+// else{
+//     console.log("normal pelo menos")
+// }
+
+// let frase = prompt("")
+
+// console.log(frase.toLowerCase())
+// console.log(frase.replaceAll("a", "e", "o", "u", "i"))
+
+// let frase = prompt("fale uma frase pra virar I")
+
+// let um = (frase.replace("a","i"))
+// let dois = (um.replace("e","i"))
+// let tres = (dois.replace("o","i"))
+// let quart = (tres.replace("u","i"))
+
+// console.log (quart)
+
+// alert("ALERTA ALERTA KRL")
+// confirm("CONFIRMA AQUI KRL  ")
+
+// let texto = document.querySelector('p');
+// let botao = document.getElementById('botao');
+// let botao2 = document.getElementById('botao2');
+
+// botao.addEventListener("click",
+// function(){
+//     window.location.href = "vai.html";
+
+// });
+
+// botao2.addEventListener("click",
+// function(){
+//     texto.textContent = "ola mundao";
+
+// });
+
+
+// const arrayLista = ["escolha outro amigão","ovo","coca","agua","pizza","hamburger","pão"]
+
+// const escolha = prompt("olá viajante, temos ovo,coca,agua,pizza,hamburger e pão, escreva o numero da posição do item q deseja, posição de 1 a 6")
+
+// if (escolha == 0){
+//     console.log(arrayLista[0])
+//    const ALERTA = alert("escolha outro amigão")
+  
+// }
+
+// if (escolha == 1){
+//     console.log(arrayLista[1])
+
+// }
+
+// if (escolha == 2){
+//     console.log(arrayLista[2])
+
+// }
+
+// if (escolha == 3){
+//     console.log(arrayLista[3])
+
+// }
+
+// if (escolha == 4){
+//     console.log(arrayLista[4])
+
+// }
+
+// if (escolha == 5){
+//     console.log(arrayLista[5])
+
+// }
+
+// if (escolha == 6){
+//     console.log(arrayLista[6])
+
+// }
+
+// console.log(arrayLista.length)
+// console.log(arrayLista.push("oi","tchau"))
+// console.log(arrayLista.pop())
+// console.log(arrayLista)
+// console.log(arrayLista.splice(2,2)) 
+// console.log(arrayLista)
+
+// var input = require('fs').readFileSync('stdin', 'utf8');
+// var lines = input.split(' ').map(x => parseInt(x));  
+
+// var a = parseInt(lines[0]);
+// var b = parseInt(lines[1]);
+// var c = parseInt(lines[2]);
+// var d = parseInt(lines[3]);
+
+// if(b > c && d> a && (c + d) > (a + b) && c > 0  && d > 0 && (a%2===0) ){
+//  console.log("valores aceito");
+
+// }
+// else{
+//     console.log("Valores não aceito");
+// }
+
+// const altura = Number(prompt("altura"))
+// const largura = Number(prompt("largura"))
+
+// function calculaArea(altura,largura){
+//     const area = altura * largura
+//     return area
+
+// }
+
+// console.log(calculaArea(altura, largura))
+
+function mundo(){
+    console.log("ola mundin")
+
+
+}
+mundo()
